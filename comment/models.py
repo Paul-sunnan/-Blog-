6683ca9a763ad01cb46d1f0c6c756c3a7ad15848
孙楠 <7773411+paul7sk8@user.noreply.gitee.com>
2020-12-31 17:06:46 +0800

    多级评论共功能

--- comment/models.py
+++ comment/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from sunnanblog.models import ArticlePost
 from django.urls import reverse
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 # 博文的评论
-class Comment(models.Model):
+class Comment(MPTTModel):
     article = models.ForeignKey(
         ArticlePost,
         on_delete=models.CASCADE,
@@ -17,9 +18,26 @@ class Comment(models.Model):
         related_name='comments'
     )
     body = models.TextField()
+
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='children'
+    )
+
+    # 新增，记录二级评论回复给谁, str
+    reply_to = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replyers'
+    )
     created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
+    class MPTTMeta:
         ordering = ('-created',)
 
     def __str__(self):
