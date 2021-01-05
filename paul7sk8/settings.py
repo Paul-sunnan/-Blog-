50175c3457aa1554b59f166047c4937d764787ba
孙楠 <7773411+paul7sk8@user.noreply.gitee.com>
2021-01-05 22:37:19 +0800

    修改ALLOWED_HOSTS

--- paul7sk8/settings.py
+++ paul7sk8/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('sunnanblog_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
